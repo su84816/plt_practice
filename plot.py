4efed76f9160570315f803c5849fdb3eac8935c3
su84816 <91594949+su84816@users.noreply.github.com>
2022-01-07 02:33:50 +0800

    Github_push0107

---
+++ plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy
+x = [1,2,3,4,5]
+y = [5,4,3,2,1]
+plt.plot(x,y,c='black',label='score1')
+plt.legend(loc='center right')
+plt.title('12/29')
+plt.xlabel('age')
+plt.xlabel('salary')
+
+plt.show
